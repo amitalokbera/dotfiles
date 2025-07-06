5668fa49c1838c9e3355c4bc08d2eb87f18903b8
Amit Bera <amitalokbera@gmail.com>
2025-07-06 16:57:50 +0530

    feat: add highlighting color plugin

---
+++ .config/nvim/lua/plugins/highlight-color.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "brenoprata10/nvim-highlight-colors",
+    event = "VeryLazy",
+    opts = {
+      render = "virtualtext", -- or "background", "foreground"
+      enable_named_colors = true,
+      -- More options available—check the repo readme
+    },
+  },
+}
